7002cec8d0bc6e031931feebe7a2540be05247e4
Dan Adams <mr.danadams@gmail.com>
2012-03-10 16:06:47 -0500

    Initial app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xilften::Application.config.secret_token = 'd37d6fafc6394684408c40866f43586b99e3f50e6bac6f180bb2ce786f35b3af23b8c1755e6a6cc2fc76435c446d8e3e899cff07e5d1f26ab6a9ce783a209fcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Xilften::Application.config.session_store :cookie_store, key: '_xilften_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Xilften::Application.config.session_store :active_record_store
